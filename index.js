36599df838084780743dd0aca9d97a4ad1932a73
ChinyONE <chiny1@web.de>
2023-09-10 08:40:17 +0200

    added different modes

--- index.js
+++ index.js
@@ -8,39 +8,50 @@ const messageStyle = {
 };
 let isSentMessage = true;
 
-document.addEventListener('onLoad', function(obj) {
+document.addEventListener('onLoad', function (obj) {
     // obj will be empty for chat widget
     // this will fire only once when the widget loads
 });
 
-document.addEventListener('onEventReceived', function(obj) {
+document.addEventListener('onEventReceived', function (obj) {
     currentObj = obj;
     console.log(currentObj);
 
     let messageId = currentObj.detail.messageId;
+    let currentMessageObj = $('*[data-id="' + messageId + '"]');
 
-    if(!lastObj.detail) {
+    // //1. Mod and Owner messages displayed as sent
+    // console.log(currentMessageObj);
+    // if (currentObj.detail.payload.tags.mod === "1" || currentObj.detail.owner) {
+    //     //
+    // } else {
+    //     currentMessageObj.toggleClass('' + messageStyle.sent + ' ' + messageStyle.received);
+    // }
+
+    //2. Only Owner message displayed as sent
+    console.log(currentMessageObj);
+    if (currentObj.detail.owner) {
+        //
+    } else {
+        currentMessageObj.toggleClass('' + messageStyle.sent + ' ' + messageStyle.received);
+    }
+
+    if (!lastObj.detail) {
         lastObj = currentObj;
         return;
     }
 
-    if(currentObj.detail.from !== lastObj.detail.from) {
-        isSentMessage = !isSentMessage; //flip bool
-    }
-
-    //change style
-    changeMessageStyleCss(messageId, isSentMessage);
+    // //3. switch between sent and received message style, if username is different from message before...
+    // if (currentObj.detail.from !== lastObj.detail.from) {
+    //     isSentMessage = !isSentMessage;
+    // }
+    //
+    // if (!isSent) {
+    //     currentMessageObj.addClass('' + messageStyle.sent + '');
+    // } else {
+    //     currentMessageObj.addClass('' + messageStyle.received + '');
+    // }
 
     //set last message from
     lastObj = currentObj;
 });
-
-function changeMessageStyleCss(messageId, isSent) {
-    //get element by message Id
-    let currentMessageObj = $('*[data-id="'+messageId+'"]');
-    console.log(currentMessageObj);
-
-    if(!isSent) {
-        currentMessageObj.toggleClass(''+messageStyle.sent+' '+messageStyle.received);
-    }
-}
